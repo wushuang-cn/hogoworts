602ef897f08605de9d4c19ba7156d2bee9bb29a4
wushuang <wushuang@qq.com>
2021-04-16 18:32:38 +0800

    企业微信-第一次作业

--- UI_Framework/testcases/conftest.py
+++ UI_Framework/testcases/conftest.py
@@ -13,15 +13,15 @@ import pytest
 from UI_Framework.pages.log import loginit
 
 
-@pytest.fixture(scope="module",autouse=True)
-def luping():
-    loginit()# 对日志初始化
-
-    #windows将cmd的编码格式设置为utf-8
-    os.system('chcp 65001')
-    cmd='scrcpy -Nr ./tmp.mp4'
-    #执行cmd终端命令
-    p= subprocess.Popen(cmd,shell=True)
-    yield
-    #操作后 杀掉进程
-    os.kill(p.pid,signal.CTRL_C_EVENT)
+# @pytest.fixture(scope="module",autouse=True)
+# def luping():
+#     loginit()# 对日志初始化
+#
+#     #windows将cmd的编码格式设置为utf-8
+#     os.system('chcp 65001')
+#     cmd='scrcpy -Nr ./tmp.mp4'
+#     #执行cmd终端命令
+#     p= subprocess.Popen(cmd,shell=True)
+#     yield
+#     #操作后 杀掉进程
+#     os.kill(p.pid,signal.CTRL_C_EVENT)
